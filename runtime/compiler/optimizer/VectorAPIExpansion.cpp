e5e25be1c37a4c3daa8d0c72bca8e95bc02172c7
midronij <jackie.midroni@ibm.com>
2022-04-20 10:48:48 -0400

    Handle vmax IL Opcode (added in omr)
    Signed-off-by: midronij <jackie.midroni@ibm.com>

--- runtime/compiler/optimizer/VectorAPIExpansion.cpp
+++ runtime/compiler/optimizer/VectorAPIExpansion.cpp
@@ -1571,7 +1571,7 @@ TR::ILOpCodes TR_VectorAPIExpansion::ILOpcodeFromVectorAPIOpcode(int32_t vectorA
       case VECTOR_OP_MUL: return scalar ? TR::ILOpCode::multiplyOpCode(elementType) : TR::vmul;
       case VECTOR_OP_DIV: return scalar ? TR::ILOpCode::divideOpCode(elementType) : TR::vdiv;
       case VECTOR_OP_MIN: return scalar ? TR::BadILOp : TR::ILOpCode::createVectorOpCode(OMR::vmin, TR::DataType::createVectorType(elementType.getDataType(), vectorLength)).getOpCodeValue();
-      case VECTOR_OP_MAX: return TR::BadILOp;
+      case VECTOR_OP_MAX: return scalar ? TR::BadILOp : TR::ILOpCode::createVectorOpCode(OMR::vmax, TR::DataType::createVectorType(elementType.getDataType(), vectorLength)).getOpCodeValue();
       case VECTOR_OP_AND: return scalar ? TR::ILOpCode::andOpCode(elementType) : TR::vand;
       case VECTOR_OP_OR:  return scalar ? TR::ILOpCode::orOpCode(elementType)  : TR::vor;
       case VECTOR_OP_XOR: return scalar ? TR::ILOpCode::xorOpCode(elementType) : TR::vxor;
