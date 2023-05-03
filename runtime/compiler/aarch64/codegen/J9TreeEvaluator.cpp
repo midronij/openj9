5ad93504945d32567284f2222750375e1ac9c2f3
Shubham Verma <shubhamv.sv@gmail.com>
2023-05-03 14:58:17 -0400

    ToEnable: Disable inlining of unsafe compare and swap
    - Disable unsafe compare and swap on aarch64
    - Disable unsafe compare and swap translation when lowering trees
    - Disable unsafe compare and swap acceleration on power
    - Disable inlining of unsafe compare and swap native and object native on x
    - Disable inlining of unsafe compare and swap on z
    
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- runtime/compiler/aarch64/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/aarch64/codegen/J9TreeEvaluator.cpp
@@ -6027,7 +6027,9 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
             if (!methodSymbol->isNative())
                break;
 
-            if ((node->isUnsafeGetPutCASCallOnNonArray() || !TR::Compiler->om.canGenerateArraylets()) && node->isSafeForCGToFastPathUnsafeCall())
+            // When dealing with array object; don't inline if arraylets or off heap is enabled
+            if (node->isSafeForCGToFastPathUnsafeCall()
+               && (node->isUnsafeGetPutCASCallOnNonArray() || (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled())))
                {
                resultReg = VMinlineCompareAndSwap(node, cg, false);
                return true;
@@ -6041,7 +6043,9 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
             if (!methodSymbol->isNative())
                break;
 
-            if ((node->isUnsafeGetPutCASCallOnNonArray() || !TR::Compiler->om.canGenerateArraylets()) && node->isSafeForCGToFastPathUnsafeCall())
+            // When dealing with array object; don't inline if arraylets or off heap is enabled
+            if (node->isSafeForCGToFastPathUnsafeCall()
+               && (node->isUnsafeGetPutCASCallOnNonArray() || (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled())))
                {
                resultReg = VMinlineCompareAndSwap(node, cg, true);
                return true;
@@ -6054,7 +6058,9 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
             if (!methodSymbol->isNative())
                break;
 
-            if ((node->isUnsafeGetPutCASCallOnNonArray() || !TR::Compiler->om.canGenerateArraylets()) && node->isSafeForCGToFastPathUnsafeCall())
+            // When dealing with array object; don't inline if arraylets or off heap is enabled
+            if (node->isSafeForCGToFastPathUnsafeCall()
+               && (node->isUnsafeGetPutCASCallOnNonArray() || (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled())))
                {
                resultReg = VMinlineCompareAndSwapObject(node, cg);
                return true;
