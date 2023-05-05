18cff0204237558c75f037ccff98187098c07a60
Shubham Verma <shubhamv.sv@gmail.com>
2023-05-05 12:11:12 -0400

    ToEnable: Disable CRC32 transformations
    Disabling these for now but will enable them once they have been taught about
    dataAddr field. These can be updated to use dataAddr field.
    
    z:
    java_util_zip_CRC32C_updateBytes
    java_util_zip_CRC32C_updateDirectByteBuffer
    
    p:
    java_util_zip_CRC32C_updateBytes
    java_util_zip_CRC32_update
    java_util_zip_CRC32_updateBytes0 | java_util_zip_CRC32_updateBytes
    java_util_zip_CRC32_updateByteBuffer0 | java_util_zip_CRC32_updateByteBuffer
    
    aarch64:
    java_util_zip_CRC32_update
    java_util_zip_CRC32_updateBytes0 | java_util_zip_CRC32_updateBytes
    java_util_zip_CRC32C_updateBytes
    java_util_zip_CRC32_updateByteBuffer0 | java_util_zip_CRC32_updateByteBuffer
    java_util_zip_CRC32C_updateDirectByteBuffer
    
    Signed-off-by: Shubham Verma <shubhamv.sv@gmail.com>

--- runtime/compiler/aarch64/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/aarch64/codegen/J9TreeEvaluator.cpp
@@ -6929,7 +6929,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
 #ifndef OSX
          case TR::java_util_zip_CRC32_update:
             {
-            if (cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && (!disableCRC32))
+            if (cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && !disableCRC32)
                {
                resultReg = inlineCRC32UpdateOneByte(node, cg);
                return true;
@@ -6943,7 +6943,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
          case TR::java_util_zip_CRC32_updateBytes:
 #endif
             {
-            if ((!TR::Compiler->om.canGenerateArraylets()) && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && (!disableCRC32))
+            if (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled() && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && !disableCRC32)
                {
                resultReg = inlineCRC32CUpdateBytes(node, cg, false, false);
                return true;
@@ -6954,7 +6954,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
 
          case TR::java_util_zip_CRC32C_updateBytes:
             {
-            if ((!TR::Compiler->om.canGenerateArraylets()) && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && (!disableCRC32))
+            if (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled() && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && !disableCRC32)
                {
                resultReg = inlineCRC32CUpdateBytes(node, cg, false, true);
                return true;
@@ -6969,7 +6969,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
          case TR::java_util_zip_CRC32_updateByteBuffer:
 #endif
             {
-            if ((!TR::Compiler->om.canGenerateArraylets()) && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && (!disableCRC32))
+            if (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled() && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && !disableCRC32)
                {
                resultReg = inlineCRC32CUpdateBytes(node, cg, true, false);
                return true;
@@ -6980,7 +6980,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
 
          case TR::java_util_zip_CRC32C_updateDirectByteBuffer:
             {
-            if ((!TR::Compiler->om.canGenerateArraylets()) && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && (!disableCRC32))
+            if (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled() && cg->comp()->target().cpu.supportsFeature(OMR_FEATURE_ARM64_CRC32) && !disableCRC32)
                {
                resultReg = inlineCRC32CUpdateBytes(node, cg, true, true);
                return true;
