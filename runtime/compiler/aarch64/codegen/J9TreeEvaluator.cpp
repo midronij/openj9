ed557a6c39c0e9e135da43f85934d57734dba709
midronij <jackie.midroni@ibm.com>
2023-11-09 09:22:00 -0500

    Revert "ToEnable: Disable inlining of unsafe compare and swap"
    This reverts commit 3e30441230b5958c41839c4b76ad4468e7d7260c.

--- runtime/compiler/aarch64/codegen/J9TreeEvaluator.cpp
+++ runtime/compiler/aarch64/codegen/J9TreeEvaluator.cpp
@@ -6031,9 +6031,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
             if (!methodSymbol->isNative())
                break;
 
-            // When dealing with array object; don't inline if arraylets or off heap is enabled
-            if (node->isSafeForCGToFastPathUnsafeCall()
-               && (node->isUnsafeGetPutCASCallOnNonArray() || (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled())))
+            if ((node->isUnsafeGetPutCASCallOnNonArray() || !TR::Compiler->om.canGenerateArraylets()) && node->isSafeForCGToFastPathUnsafeCall())
                {
                resultReg = VMinlineCompareAndSwap(node, cg, false);
                return true;
@@ -6047,9 +6045,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
             if (!methodSymbol->isNative())
                break;
 
-            // When dealing with array object; don't inline if arraylets or off heap is enabled
-            if (node->isSafeForCGToFastPathUnsafeCall()
-               && (node->isUnsafeGetPutCASCallOnNonArray() || (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled())))
+            if ((node->isUnsafeGetPutCASCallOnNonArray() || !TR::Compiler->om.canGenerateArraylets()) && node->isSafeForCGToFastPathUnsafeCall())
                {
                resultReg = VMinlineCompareAndSwap(node, cg, true);
                return true;
@@ -6062,9 +6058,7 @@ J9::ARM64::CodeGenerator::inlineDirectCall(TR::Node *node, TR::Register *&result
             if (!methodSymbol->isNative())
                break;
 
-            // When dealing with array object; don't inline if arraylets or off heap is enabled
-            if (node->isSafeForCGToFastPathUnsafeCall()
-               && (node->isUnsafeGetPutCASCallOnNonArray() || (!TR::Compiler->om.canGenerateArraylets() && !TR::Compiler->om.isOffHeapAllocationEnabled())))
+            if ((node->isUnsafeGetPutCASCallOnNonArray() || !TR::Compiler->om.canGenerateArraylets()) && node->isSafeForCGToFastPathUnsafeCall())
                {
                resultReg = VMinlineCompareAndSwapObject(node, cg);
                return true;
