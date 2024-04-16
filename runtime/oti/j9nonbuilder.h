3cf9836c6a60dd2e7f019a04ba0821cc760f41be
tajila <atobia@ca.ibm.com>
2024-04-16 10:03:23 -0400

    Array base offset zero
    Signed-off-by: tajila <atobia@ca.ibm.com>

--- runtime/oti/j9nonbuilder.h
+++ runtime/oti/j9nonbuilder.h
@@ -5427,6 +5427,7 @@ typedef struct J9VMThread {
 	void* gcExtensions;
 	UDATA contiguousIndexableHeaderSize;
 	UDATA discontiguousIndexableHeaderSize;
+	UDATA unsafeIndexableHeaderSize;
 #if defined(J9VM_ENV_DATA64)
 	UDATA isIndexableDataAddrPresent;
 #endif /* defined(J9VM_ENV_DATA64) */
@@ -5604,6 +5605,7 @@ typedef struct J9VMThread {
 #define J9VMTHREAD_OBJECT_HEADER_SIZE(vmThread) (J9VMTHREAD_COMPRESS_OBJECT_REFERENCES(vmThread) ? sizeof(J9ObjectCompressed) : sizeof(J9ObjectFull))
 #define J9VMTHREAD_CONTIGUOUS_INDEXABLE_HEADER_SIZE(vmThread) ((vmThread)->contiguousIndexableHeaderSize)
 #define J9VMTHREAD_DISCONTIGUOUS_INDEXABLE_HEADER_SIZE(vmThread) ((vmThread)->discontiguousIndexableHeaderSize)
+#define J9VMTHREAD_UNSAFE_INDEXABLE_HEADER_SIZE(vmThread) ((vmThread)->unsafeIndexableHeaderSize)
 
 typedef struct JFRState {
 	char *jfrFileName;
@@ -5973,6 +5975,7 @@ typedef struct J9JavaVM {
 	UDATA arrayletLeafLogSize;
 	UDATA contiguousIndexableHeaderSize;
 	UDATA discontiguousIndexableHeaderSize;
+	UDATA unsafeIndexableHeaderSize;
 #if defined(J9VM_ENV_DATA64)
 	UDATA isIndexableDataAddrPresent;
 	BOOLEAN isIndexableDualHeaderShapeEnabled;
